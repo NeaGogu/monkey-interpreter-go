69a248f37a457d5f758a042187a8e03cc063f0c4
vlad <vlad.plaiasu.vp@gmail.com>
2024-09-23 20:14:22 +0200

    idk but I need to return the laptop

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,9 @@
 package ast
 
-import "NeaGogu/monkey-interpreter/token"
+import (
+	"NeaGogu/monkey-interpreter/token"
+	"bytes"
+)
 
 // let y = 15;
 
@@ -11,6 +14,7 @@ import "NeaGogu/monkey-interpreter/token"
 type Node interface {
 	// will be used mainly for debugging and testing
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -36,6 +40,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
+// ----------- let statements
+
 // let a = 123
 type LetStatement struct {
 	Value Expression  // 123
@@ -45,6 +59,22 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral())
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	// will be removed after we know how to parse expressions
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
 
 type Identifier struct {
 	Token token.Token // the token.IDENT token
@@ -60,6 +90,10 @@ type Identifier struct {
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
+func (i *Identifier) String() string {
+	return i.Value
+}
+
 // --- return statements
 
 type ReturnStatement struct {
@@ -69,3 +103,37 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	// will be removed after we know how to parse expressions
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
+// -------- expressions
+
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+// the main reason we have an "expressioon statement" is so we can add it to the
+// statement slice of the Program
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
